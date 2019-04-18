5da2b2aa53888f851f050f730a783f282fec0b93
nola222 <1422450420@qq.com>
2019-04-18 18:34:33 +0800

    update settings

--- batch_upload/batch_upload/settings.py
+++ batch_upload/batch_upload/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nqlcr0cy#^eq)bvz49$hilco_li(%u)+zyja%nd6qfz&ubxl3a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'batch_upload.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'upload_test',
+        'USER': 'root',
+        'PASSWORD': 'nola123456',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
@@ -103,15 +107,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
